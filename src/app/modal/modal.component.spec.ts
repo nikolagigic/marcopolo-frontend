f59b6da22da827cd299df51f570b9a5d5eb4c0d5
Nikola Gigic <nik.gigic@gmail.com>
2021-07-16 10:46:57 +0200

    added ProductInterface in modal

--- src/app/modal/modal.component.spec.ts
+++ src/app/modal/modal.component.spec.ts
@@ -1,20 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModalComponent } from './modal.component';
+import { NgbdModalContent } from './modal.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from '../app.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { ProductsComponent } from '../products/products.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('ModalComponent', () => {
-  let component: ModalComponent;
-  let fixture: ComponentFixture<ModalComponent>;
+  let component: NgbdModalContent;
+  let fixture: ComponentFixture<NgbdModalContent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
+      declarations: [
+        AppComponent,
+        RegisterComponent,
+        LoginComponent,
+        ProductsComponent,
+        NgbdModalContent
+      ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        NgbModule
+      ],
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ModalComponent);
+    fixture = TestBed.createComponent(NgbdModalContent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+import { NgbdModalContent } from '../modal/modal.component';
+import { BrowserModule } from '@angular/platform-browser';
 
-describe('ProductsComponent', () => {
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from '../app.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { AppRoutingModule } from '../app-routing.module';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+fdescribe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [
+        AppComponent,
+        RegisterComponent,
+        LoginComponent,
+        ProductsComponent,
+        NgbdModalContent
+      ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        NgbModule
+      ],
     })
     .compileComponents();
   });
@@ -21,5 +46,6 @@ describe('ProductsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    console.log(component.products);
   });
 });
